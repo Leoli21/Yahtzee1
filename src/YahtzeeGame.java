53de0599762dfee2ff0e83ace711813d0d4bd08e
Leoli21 <43002601+Leoli21@users.noreply.github.com>
2019-06-11 09:33:00 -0400

    Add files via upload

--- src/YahtzeeGame.java
+++ src/YahtzeeGame.java
@@ -12,131 +12,25 @@ public class YahtzeeGame {
 		//Enter die number(s) that user want to keep
 		System.out.println("Enter die number(s) to keep (separated by a space): ");
 		String dieKeep = input.nextLine();
-		for (int i = 0; i <dieKeep.length(); i++)
+		if(!dieKeep.contains("1"))
 		{
-			//Contain 1 first, and maybe 2, 3, 4, 5
-			if (dieKeep.contains("1"))
-			{
-				if(dieKeep.contains("2"))
-				{
-					if(dieKeep.contains("3"))
-					{
-						if (dieKeep.contains("4"))
-						{
-							if (dieKeep.contains("5"))
-							{
-								break;
-							}
-							else
-							{
-								yahtzee.rollADice(5);
-							}
-						}
-						else
-						{
-							yahtzee.rollADice(4);
-							yahtzee.rollADice(5);
-						}
-					}
-					else
-					{
-						yahtzee.rollADice(3);
-						yahtzee.rollADice(4);
-						yahtzee.rollADice(5);
-					}
-				}
-				else
-				{
-					yahtzee.rollADice(2);
-					yahtzee.rollADice(3);
-					yahtzee.rollADice(4);
-					yahtzee.rollADice(5);
-				}
-			}
-			//Does not contain 1, but does contain 2, 3, 4, or 5
-			else if(dieKeep.contains("2"))
-			{  
-				yahtzee.rollADice(1);
-				if(dieKeep.contains("3"))
-				{
-					if(dieKeep.contains("4"))
-					{
-						if(dieKeep.contains("5"))
-						{
-							break;
-						}
-						else
-						{
-							yahtzee.rollADice(5);
-						}
-					}
-					else
-					{
-						yahtzee.rollADice(4);
-						yahtzee.rollADice(5);
-					}
-				}
-				else
-				{
-					yahtzee.rollADice(3);
-					yahtzee.rollADice(4);
-					yahtzee.rollADice(5);
-				}
-				yahtzee.rollADice(3);
-				yahtzee.rollADice(4);
-				yahtzee.rollADice(5);
-			}
-			
-			//Does not contain 1 or 2, but does contain 3 or 4 or 5
-			else if(dieKeep.contains("3"))
-			{
-				yahtzee.rollADice(1);
-				yahtzee.rollADice(2);
-				if(dieKeep.contains("4"))
-				{
-					if(dieKeep.contains("5"))
-					{
-						break;
-					}
-					else
-					{
-						yahtzee.rollADice(5);
-					}
-				}
-				else
-				{
-					yahtzee.rollADice(4);
-					yahtzee.rollADice(5);
-				}
-				yahtzee.rollADice(4);
-				yahtzee.rollADice(5);
-			}
-			
-			//Does not contain 1,2, or 3, but does contain 4 or 5
-			else if(dieKeep.contains("4"))
-			{
-				yahtzee.rollADice(1);
-				yahtzee.rollADice(2);
-				yahtzee.rollADice(3);
-				if(dieKeep.contains("5"))
-				{
-					break;
-				}
-				else
-				{
-					yahtzee.rollADice(5);
-				}
-				yahtzee.rollADice(5);
-			}
-			
-			//Does not contain 1,2,3 and 4, but does contain 5
-			else if(dieKeep.contains("5"))
-			{
-				yahtzee.rollADice(1);
-				yahtzee.rollADice(2);
-				yahtzee.rollADice(3);
-				yahtzee.rollADice(4);
-			}
+			yahtzee.rollADice(1);
+		}
+		else if(!dieKeep.contains("2"))
+		{
+			yahtzee.rollADice(2);
+		}
+		else if(!dieKeep.contains("3"))
+		{
+			yahtzee.rollADice(3);
+		}
+		else if(!dieKeep.contains("4"))
+		{
+			yahtzee.rollADice(4);
+		}
+		else if(!dieKeep.contains("5"))
+		{
+			yahtzee.rollADice(5);
 		}
 
 		System.out.println(yahtzee.getScoreCard());
